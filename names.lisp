2ddc0be4eacccd86e655cce6097fc57101301019
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2017-04-23 12:31:18 +0200

    minor

--- names.lisp
+++ names.lisp
@@ -13,11 +13,45 @@ exec ros -Q -- $0 "$@"
 (in-package :ros.script.names.3685353031)
 
 
+(defparameter *orig* "names.db")
+(defparameter *remaining* "results.db")
+
+(defvar *db* '())
+
+
+(defun save-db (filename)
+  "Save current *db* to file."
+  (with-open-file (out filename
+                       :direction :output
+                       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun random-elt (choices)
+  "Choose an element from a list at random."
+  (elt choices (random (length choices))))
+
+(defun one-of (set)
+  "Pick one element of set, and make a list of it."
+  (list (random-elt set)))
+
+
 (defun get-file (filename)
   (with-open-file (stream filename)
     (loop for line = (read-line stream nil)
           while line
           collect line)))
+
+
+(defun print-n-names (n)
+  (print n))
+
+
+(defun remove-from-db (item)
+  (setf *db* (remove item *db* :test #'string-equal)))
+
+(defun start ()
+  (setf *db* (get-file *orig*)))
 
 (defun main (&optional n &rest argv)
   (declare (ignore argv))
